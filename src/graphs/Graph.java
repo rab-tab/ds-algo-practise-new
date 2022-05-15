49d25440bce1769037b4f690b8aaaec3eab74d6a
Rabia <sdetcode@gmail.com>
2022-05-15 18:39:21 +0530

    graph dfs implementation

--- src/graphs/Graph.java
+++ src/graphs/Graph.java
@@ -50,5 +50,34 @@ public class Graph {
             }
         }
     }
+
+    void DFS(int s) {
+        // Mark all the vertices as
+        // not visited(set as
+        // false by default in java)
+        boolean visited[] = new boolean[V];
+
+        // Call the recursive helper
+        // function to print DFS
+        // traversal
+        DFSUtil(s, visited);
+    }
+
+    // A function used by DFS
+    void DFSUtil(int v, boolean visited[]) {
+        // Mark the current node as visited and print it
+        visited[v] = true;
+        System.out.print(v + " ");
+
+        // Recur for all the vertices adjacent to this
+        // vertex
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) {
+            int n = i.next();
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+
 }
 
