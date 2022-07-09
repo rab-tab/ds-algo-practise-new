f97c1513cf296db1ad1999ea62d692ca6bc81296
Rabia <sdetcode@gmail.com>
2022-07-10 01:24:55 +0530

    "String permutations"

---
+++ src/recursion/StringPermutations.java
@@ -0,0 +1,24 @@
+package recursion;
+
+public class StringPermutations {
+    public static void main(String[] args) {
+        String s = "abc";
+        permute(s, "");
+    }
+
+    private static void permute(String s, String ans) {
+
+        if (s.length()==0)
+        {
+            System.out.println(ans);
+            return;
+        }
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+                String left = s.substring(0, i);
+                String right = s.substring(i + 1);
+                permute(left + right, ans + ch);
+
+            }
+    }
+}
