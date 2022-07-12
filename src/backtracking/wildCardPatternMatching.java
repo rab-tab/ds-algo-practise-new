e44c5aa993b83ba364a22f87af0e7b8b101a23c1
Rabia <sdetcode@gmail.com>
2022-07-12 17:19:47 +0530

    "wild card pattern matching recursive approach"

---
+++ src/backtracking/wildCardPatternMatching.java
@@ -0,0 +1,36 @@
+package backtracking;
+
+public class wildCardPatternMatching {
+    public static void main(String[] args) {
+        String s = "summer";
+        String pattern = "s*e?";
+        isMatch(s, pattern, s.length(), pattern.length(), 0, 0);
+        //? - any one character
+        //* -0 or more  chars
+        isMatch(s, pattern, s.length(), pattern.length(), 0, 0);
+        System.out.println(isMatch(s, pattern, s.length(), pattern.length(), 0, 0));
+    }
+
+    private static boolean isMatch(String s, String pattern, int strLen, int patternLen,
+                                   int strIndex, int patternIndex) {
+        if (strIndex == strLen && patternIndex == patternLen)
+            return true;
+        if (strIndex == strLen ) {
+            return false;
+        }
+        if(patternIndex == patternLen ) return false;
+        if( s.charAt(strIndex) != pattern.charAt(patternIndex)) return false;
+
+        if (s.charAt(strIndex) == pattern.charAt(patternIndex) || pattern.charAt(patternIndex) == '?') {
+            return isMatch(s, pattern, strLen, patternLen, strIndex + 1, patternIndex + 1);
+        }
+        if (pattern.charAt(patternIndex) == '*') {
+            return (isMatch(s, pattern, strLen, patternLen, strIndex + 1, patternIndex) ||
+                    isMatch(s, pattern, strLen, patternLen, strIndex, patternIndex + 1));
+        }
+        return false;
+
+    }
+
+
+}
