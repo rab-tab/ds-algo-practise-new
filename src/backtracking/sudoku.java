5a176272dfa372a9bfd0aabeba0029f04d667055
Rabia <sdetcode@gmail.com>
2023-05-01 13:03:39 +0530

    sudoku backtracking refactoring

--- src/backtracking/sudoku.java
+++ src/backtracking/sudoku.java
@@ -14,63 +14,75 @@ public class sudoku {
                 {1, 3, 0, 0, 0, 0, 2, 5, 0},
                 {0, 0, 0, 0, 0, 0, 0, 7, 4},
                 {0, 0, 5, 2, 0, 6, 3, 0, 0}};
-        solve(grid, 0, 0, n, "");
+        solve(grid, n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 
-    public static void solve(int[][] grid, int row, int col, int gridSize, String ans) {
+    public static boolean solve(int[][] grid, int n) {
         //cell should not be zero
         //for every row place 1-9 in each column and check if its safe
         //if yes place the number and continue
         //if no return
         // increase the row number and continue
-        if (row < 0 || col < 0) return;
-        if (row == grid.length-1) {
+       /* if (row < 0 || col < 0) return;
+        if (row == grid.length - 1) {
             System.out.println(ans);
             return;
         }
-        if (col == grid[0].length-1) {
+        if (col == grid[0].length - 1) {
             row = row + 1;
             col = 0;
         } else {
             col = col + 1;
-        }
+        }*/
 
-        if (grid[row][col] != 0) {
-            solve(grid, row, col, gridSize, ans);
-        }
-        for (int currCol = 1; currCol <= gridSize; currCol++) {
-            if (isSafe(grid, row, col, currCol)) {
-                grid[row][col] = col;
-                solve(grid, row, col, gridSize, ans + "--" + currCol);
-                grid[row][col] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //check for empty cell
+                if (grid[i][j] == 0) {
+                    for (int value = 1; value <= 9; value++) {
+                        if (isSafe(grid, i, j, value)) {
+                            grid[i][j] = value;
+                            boolean ans = solve(grid, n);
+                            if (ans == true) return true;
+                            grid[i][j] = 0;
+                        }
+
+                    }
+                    return false;
+                }
             }
         }
-
-
+        return true;
     }
 
-    private static boolean isSafe(int[][] grid, int row, int col, int data) {
+    private static boolean isSafe(int[][] grid, int i, int j, int value) {
         //same column
         for (int currRow = 0; currRow < grid.length; currRow++) {
-            if (grid[currRow][col] == data) {
-           //     System.out.println("same column contains " + data);
+            if (grid[currRow][j] == value) {
+                //     System.out.println("same column contains " + data);
                 return false;
             }
         }
         // same row
         for (int currCol = 0; currCol < grid.length; currCol++) {
-            if (grid[row][currCol] == data) return false;
+            if (grid[i][currCol] == value) return false;
         }
 
-        int subMatrixRow = row / 3 * 3;
-        int subMatrixColumn = col / 3 * 3;
+        int subMatrixRow = i / 3 * 3;
+        int subMatrixColumn = j / 3 * 3;
 
 
         //sub matrix
         for (int currRow = 0; currRow < 3; currRow++) {
             for (int currCol = 0; currCol < 3; currCol++) {
-                if (grid[subMatrixRow + currRow][subMatrixColumn + currCol] == data) return false;
+                if (grid[subMatrixRow + currRow][subMatrixColumn + currCol] == value) return false;
             }
         }
 
