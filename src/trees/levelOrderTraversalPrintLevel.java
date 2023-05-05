7c90c37a5d93b66cdf5df8c1a09cb3d895f1de45
Rabia <sdetcode@gmail.com>
2023-05-05 22:38:19 +0530

    trees level order traversal

---
+++ src/trees/levelOrderTraversalPrintLevel.java
@@ -0,0 +1,40 @@
+package trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class levelOrderTraversalPrintLevel {
+    public static void main(String[] args) {
+        Node root = new Node(3);
+        root.left = new Node(2);
+        root.right = new Node(5);
+        root.left.left = new Node(1);
+        root.left.right = new Node(4);
+        traverse(root);
+    }
+
+    private static void traverse(Node root) {
+        if (root == null) return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Node temp = q.peek();
+            q.remove();
+
+            if (temp == null) {
+                System.out.println();
+                if (!q.isEmpty()) {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(temp.data + " ");
+                if (temp.left != null) q.add(temp.left);
+                if (temp.right != null) q.add(temp.right);
+            }
+
+        }
+
+    }
+}
