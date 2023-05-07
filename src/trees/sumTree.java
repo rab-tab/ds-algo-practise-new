aac0264bbeb9914310e66ed10d66b1a157136da1
Rabia <sdetcode@gmail.com>
2023-05-07 19:24:42 +0530

    sum Tree

--- src/trees/sumTree.java
+++ src/trees/sumTree.java
@@ -9,19 +9,18 @@ public class sumTree {
         root.left.right = new Node(-4);
         root.right.left = new Node(7);
         root.right.right = new Node(5);
-        System.out.println("total sum "+solve(root));
+        System.out.println("total sum "+solveSumTree(root));
 
     }
 
-    public static int solve(Node root){
+    public static int solveSumTree(Node root){
 
         if(root==null) return 0;
 
-       /* if(root.left==null && root.right==null) return root.data;
-        return sum+solve(root.left,root.data)+solve(root.right,root.data);*/
-        int old_val=root.data;
-        root.data=solve(root.left)+solve(root.right);
-        return old_val+root.data;
+        int leftAns=solveSumTree(root.left);
+        int rightAns=solveSumTree(root.right);
+        root.data=leftAns+rightAns+root.data;
+        return root.data;
 
     }
 }
