82caf35634237d14ac7aa86e277154b19cd4c993
Rabia <sdetcode@gmail.com>
2023-05-09 18:46:52 +0530

    reverse string recursion

---
+++ src/recursion/Strings/reverseString.java
@@ -0,0 +1,17 @@
+package recursion.Strings;
+
+public class reverseString {
+    public static void main(String[] args) {
+        String str = "performance";
+        reverse(str, str.length(), "");
+    }
+
+    private static void reverse(String str, int idx, String revStr) {
+
+        if (idx == 0) {
+            System.out.println(revStr);
+            return ;
+        }
+         reverse(str, idx -1, revStr+str.charAt(idx-1));
+    }
+}
