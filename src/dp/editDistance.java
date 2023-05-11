f6b44b9f2417b692fdf76dba1a2909a1d37e1e69
Rabia <sdetcode@gmail.com>
2023-05-11 16:28:43 +0530

    edit Distance DP

---
+++ src/dp/editDistance.java
@@ -0,0 +1,33 @@
+package dp;
+
+public class editDistance {
+    public static void main(String[] args) {
+        String s1 = "intention";
+        String s2 = "execution";
+        int[][] dp = new int[s1.length()][s2.length()];
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(solve(s1, s2, 0, 0, dp));
+
+    }
+
+    private static int solve(String s1, String s2, int i, int j, int[][] dp) {
+
+        if (i == s1.length()) return s2.length() - j;
+        if (j == s2.length()) return s1.length() - i;
+        if (dp[i][j] != -1) return dp[i][j];
+        int ans = 0;
+        if (s1.charAt(i) == s2.charAt(j))
+            ans = solve(s1, s2, i + 1, j + 1, dp);
+        else {
+            int insertAns = solve(s1, s2, i, j + 1, dp);
+            int replaceAns = solve(s1, s2, i + 1, j + 1, dp);
+            int deleteAns = solve(s1, s2, i + 1, j, dp);
+            ans = Math.min(Math.min(insertAns, replaceAns), deleteAns) + 1;
+        }
+        return dp[i][j] = ans;
+    }
+}
