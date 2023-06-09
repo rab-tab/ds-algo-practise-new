9f772351c0b9c9ffdb2c75dcbaae6c54e151205a
Rabia <sdetcode@gmail.com>
2023-06-09 20:11:35 +0530

    pair class for heaps

---
+++ src/heaps/Pair.java
@@ -0,0 +1,13 @@
+package heaps;
+
+import trees.Node;
+
+public class Pair<T, T1> {
+    Boolean ans;
+    Integer data;
+
+    public Pair(Boolean ans, Integer data) {
+        this.ans = ans;
+        this.data = data;
+    }
+}
