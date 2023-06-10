8ab58aa8d1f3f6f7315eba84d7f3e6ef80b11856
Rabia <sdetcode@gmail.com>
2023-06-10 20:49:02 +0530

    Equal Subset Sum dp - tabulation approach

--- src/dp/partitionEqualSubsetSum.java
+++ src/dp/partitionEqualSubsetSum.java
@@ -2,14 +2,14 @@ package dp;
 
 public class partitionEqualSubsetSum {
     public static void main(String[] args) {
-        int arr[] = {1, 5, 11, 5,1};
+        int arr[] = {1, 5, 11, 5};
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
 
         if (sum % 2 != 0) {
-            System.out.println("Can arr be divided into two equal partitions - top down approach --- false" );
+            System.out.println("Can arr be divided into two equal partitions - top down approach --- false");
         } else {
             int target = sum / 2;
             boolean dp[][] = new boolean[arr.length + 1][target + 1];
@@ -20,6 +20,9 @@ public class partitionEqualSubsetSum {
             }
             boolean ans = topDownApproach(0, arr, target, dp);
             System.out.println("Can arr be divided into two equal partitions - top down approach --- " + ans);
+
+            boolean ans1 = tabulation( arr, target, dp);
+            System.out.println("Can arr be divided into two equal partitions - tabulation approach --- " + ans1);
         }
 
     }
@@ -48,6 +51,25 @@ public class partitionEqualSubsetSum {
         boolean exclude = solve(index + 1, nums, target);
         return dp[index][target] = include || exclude;
 
+    }
+
+    //tabulation
+    private static boolean tabulation(int[] nums, int target, boolean[][] dp) {
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int k = nums.length - 1; k >= 0; k--) {
+            for (int t = 1; t <= target; t++) {
+                boolean include = false;
+                if (t - nums[k] >= 0)
+                    include = dp[k + 1][t - nums[k]];
+
+                boolean exclude = dp[k + 1][t];
+                dp[k][t] = include || exclude;
+            }
+        }
+        return dp[0][target];
     }
 }
 
