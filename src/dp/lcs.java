68623d035e4d40f758738472bfe02febf4809759
Rabia <sdetcode@gmail.com>
2023-06-15 21:24:57 +0530

    lcs tabulation approach

--- src/dp/lcs.java
+++ src/dp/lcs.java
@@ -4,13 +4,22 @@ public class lcs {
     public static void main(String[] args) {
         String s1 = "abcdabc";
         String s2 = "bcbcda";
-        int[][] dp = new int[s1.length()][s2.length()];
-        for (int i = 0; i < s1.length(); i++) {
-            for (int j = 0; j < s2.length(); j++) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[i].length; j++) {
                 dp[i][j] = -1;
             }
         }
-        System.out.println(topDownApproach(s1, s2, s1.length() - 1, s2.length() - 1, dp));
+        System.out.println("Top down approach --" + topDownApproach(s1, s2, s1.length() , s2.length(), dp));
+
+        int[][] dp1 = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                dp1[i][j] = 0;
+            }
+        }
+
+        System.out.println("Tabulation --" + tabulation(s1, s2, dp1));
     }
 
     public static int topDownApproach(String s1, String s2, int i, int j, int[][] dp) {
@@ -25,4 +34,19 @@ public class lcs {
         dp[i][j] = ans;
         return ans;
     }
+
+    public static int tabulation(String s1, String s2, int[][] dp) {
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            for (int j = s2.length() - 1; j >= 0; j--) {
+                int ans = 0;
+                if (s1.charAt(i) == s2.charAt(j))
+                    ans = 1 + dp[i + 1][j + 1];
+                else ans = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+
+    }
+
 }
