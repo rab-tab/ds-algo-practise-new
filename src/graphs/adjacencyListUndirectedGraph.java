6a9d1752cd0873fdacff47051404bfb68d682e72
Rabia <sdetcode@gmail.com>
2023-06-25 22:54:30 +0530

    graphs - adjacency list undirected graph representation

---
+++ src/graphs/adjacencyListUndirectedGraph.java
@@ -0,0 +1,41 @@
+package graphs;
+
+import java.util.*;
+
+public class adjacencyListUndirectedGraph {
+    static HashMap<Integer, List<Integer>> adjList = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        int totalNodes,node;
+        System.out.println("Enter total nodes ");
+        totalNodes=scanner.nextInt();
+        for(int i=0;i<totalNodes;i++){
+            System.out.println("Enter node ");
+            node=scanner.nextInt();
+            adjList.put(node,new ArrayList<>());
+        }
+        addEdge(0, 1, true);
+        addEdge(1, 2, true);
+        addEdge(0, 2, true);
+        printAdjacencyList();
+
+    }
+
+
+    static void addEdge(int u, int v, boolean directed) {
+        //directed -0 - undirected graph
+        //directed -1 - directed graph
+        adjList.get(u).add(v);
+        //undirected
+        if (directed == false)
+            adjList.get(v).add(u);
+    }
+
+    static void printAdjacencyList() {
+        for (Map.Entry<Integer, List<Integer>> map : adjList.entrySet()) {
+            System.out.println(map.getKey() + " -- " + map.getValue());
+        }
+    }
+}
