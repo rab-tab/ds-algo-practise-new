5a27ad9793c0e2171e3e951289c3fb1447696b88
Rabia <sdetcode@gmail.com>
2023-07-06 15:27:28 +0530

    house robber tabulation

--- src/recursion/houseRobber.java
+++ src/recursion/houseRobber.java
@@ -11,6 +11,10 @@ public class houseRobber {
         }
         System.out.println("DP( top down) - maximum amount of money that can be stolen--" + dpSolve(arr, arr.length - 1, dp));
 
+        int dp1[] = new int[arr.length + 1];
+        tabulation(arr, arr.length - 1, dp1);
+        System.out.println("Tabulation - maximum amount of money that can be stolen--" + tabulation(arr, arr.length - 1, dp1));
+
     }
 
     private static int solve(int[] arr, int n) {
@@ -29,9 +33,23 @@ public class houseRobber {
         if (n < 0) return 0;
         if (n == 0) return arr[0];
         if (dp[n] != -1) return dp[n];
-        int incl = dpSolve(arr, n - 2,dp) + arr[n];
-        int excl = dpSolve(arr, n - 1,dp) + 0;
+        int incl = dpSolve(arr, n - 2, dp) + arr[n];
+        int excl = dpSolve(arr, n - 1, dp) + 0;
         int ans = Math.max(incl, excl);
         return dp[n] = ans;
+    }
+
+    private static int tabulation(int arr[], int n, int[] dp1) {
+        dp1[0] = arr[0];
+        for (int i = 1; i <= n; i++) {
+            int incl = 0;
+            if (i - 2 >= 0) {
+                incl = dp1[i - 2] + arr[i];
+                int excl = dp1[i - 1] + 0;
+                dp1[i] = Math.max(incl, excl);
+            }
+
+        }
+        return dp1[n];
     }
 }
